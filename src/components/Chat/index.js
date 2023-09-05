cc5d0d14f0bca087decc7259d5aa9636bf96d714
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-05 15:57:47 +0700

    Update index.js

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,3 +1,3 @@
-import WhatsappHome from "./welcome/WhatsappHome";
+import WhatsappHome from "./Welcome/WhatsappHome";
 import ChatContainer from "./ChatContainer";
 export { WhatsappHome, ChatContainer };
