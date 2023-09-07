f34c1afd30458a8ecb51ca6ccd293ab68324ce22
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-07 20:01:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 //socket io
-const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
+const socket = io(process.env.REACT_APP_API_ENDPOINT?.split("/api/v1")[0]);
 
 function App() {
   //const [connected, setConnected] = useState(false);
