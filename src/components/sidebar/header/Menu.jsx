6dea55242ed39af00679a06443bee35dcdd5be65
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-13 14:43:21 +0700

    Update Menu.jsx

--- src/components/sidebar/header/Menu.jsx
+++ src/components/sidebar/header/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu({ setShowCreateGroup }) {
     <>
       <div className="absolute right-1 z-50 dark:bg-dark_bg_2 dark:text-dark_text_1 shadow-md w-52">
         <ul>
-          <li
+          {/* <li
             className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
             onClick={() => setShowCreateGroup(true)}
           >
@@ -22,7 +22,7 @@ export default function Menu({ setShowCreateGroup }) {
           </li>
           <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
             <span>Settings</span>
-          </li>
+          </li> */}
           <li
             className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
             onClick={() => dispatch(logout())}
